9cb0aec5459d0bf73f66e2b28caaea59c11b7d94
ResSimSRCNN <116525991+ResSimSRCNN@users.noreply.github.com>
2022-10-24 11:41:10 +1030

    Add files via upload

---
+++ model_RNN_SRCNN.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import *
+
+def Create_Model(filters=None, kernel_size=None, activations=None, num_of_steps=None):
+    if kernel_size is None:
+        kernel_size = [7, 5, 5]
+    if filters is None:
+        filters = [32, 16, 16]
+    if activations is None:
+        activations = ['relu', 'relu', 'relu']
+    if num_of_steps is None:
+        num_of_steps = 11
+
+    # inputting the permeability and saturation
+    perm_input = Input(shape=(num_of_steps, 100, 100, 2), name="permeability")
+
+    sat_input = Input(shape=(num_of_steps, 50, 50, 1), name="Average_saturation")
+
+
+    sat = Conv3DTranspose(
+        filters=1,
+        kernel_size=(1, 2, 2),
+        strides=(1, 2, 2),
+        padding="valid")(sat_input)
+
+
+    # merging the permeability with the saturation
+    merged = concatenate([perm_input, sat])
+
+    merged = ConvLSTM2D(filters=filters[0], kernel_size=kernel_size[0],
+                        padding='same', return_sequences=True, activation=activations[0])(merged)
+
+    merged = ConvLSTM2D(filters=filters[1], kernel_size=kernel_size[1],
+                        padding='same', return_sequences=True, activation=activations[1])(merged)
+
+    merged = ConvLSTM2D(filters=filters[2], kernel_size=kernel_size[2],
+                        padding='same', return_sequences=True, activation=activations[2])(merged)
+
+    output_layer = ConvLSTM2D(filters=2, kernel_size=1,
+                              padding='same', return_sequences=True, activation='relu')(merged)
+
+    # creating the model
+    model = tf.keras.Model([perm_input, sat_input],
+                           output_layer,
+                           name="SRCNN_for_reservoir_simulation")
+
+    return model
